dae59d64efb81bf1569f08af5c1792089c35bd3e
Nathaniel Tampus <nate@mugnavo.com>
2025-08-17 22:59:09 +0800

    bump deps

--- packages/auth/src/tanstack/functions.ts
+++ packages/auth/src/tanstack/functions.ts
@@ -2,7 +2,6 @@ import { createServerFn } from "@tanstack/react-start";
 import { getAuthSession } from "./utils";
 
 export const $getUser = createServerFn({ method: "GET" }).handler(async () => {
-  console.log("--------- CALLING");
   const { user } = await getAuthSession();
 
   return user;
