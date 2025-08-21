7a39473546a9584d610cb533cd8cdb26ab5fca2c
Nathaniel Tampus <nate@mugnavo.com>
2025-08-21 12:59:00 +0800

    add link

--- packages/api/src/adapters/tanstack/client.ts
+++ packages/api/src/adapters/tanstack/client.ts
@@ -30,8 +30,6 @@ const getORPCClient = createIsomorphicFn()
       url: `${window.location.origin}/api/rpc`,
     });
 
-    console.log("clieeent-sideeeeeeeeeeeeeee");
-
     return createORPCClient(link);
   });
 
