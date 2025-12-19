ff087ad1cf97767c3630f34ed6ae5d4d64b537cf
Nathaniel John Tampus <nat@nize.ph>
2025-12-19 19:30:25 +0800

    native vite tsconfig paths

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -6,6 +6,9 @@ import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
     devtools(),
     tanstackStart(),
